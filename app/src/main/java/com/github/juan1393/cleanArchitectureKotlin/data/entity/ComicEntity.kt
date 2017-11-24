e4456e4391eba711247cebf2770bb1e909ed0ab0
Juan Gómez <j.gtrapero@gmail.com>
2017-11-24 10:51:18 +0100

    Added entities

---
+++ app/src/main/java/com/github/juan1393/cleanArchitectureKotlin/data/entity/ComicEntity.kt
@@ -0,0 +1,12 @@
+package com.github.juan1393.cleanArchitectureKotlin.data.entity
+
+
+class ComicEntity(val id: Int? = null,
+                  val title: String? = null,
+                  val description: String? = null,
+                  val pageCount: Int? = null,
+                  val printPrice: Float? = null,
+                  val thumbnailUrl: String? = null,
+                  val imagesUrl: List<String>? = null,
+                  var creators: List<CreatorEntity>? = null,
+                  var characters: List<CharacterEntity>? = null)
